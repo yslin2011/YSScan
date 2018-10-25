a3d01cadff911acaadcf35dff3a66ddb2cfb4dfb
yslin <359927530@qq.com>
2018-10-25 09:43:56 +0800

    initial

---
+++ YSScanFramwork.framework/Headers/YSScan.h
@@ -0,0 +1,18 @@
+//
+//  YSScan.h
+//  YSScanFramwork
+//
+//  Created by yslin on 2018/10/25.
+//  Copyright © 2018年 yslin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YSScan : NSObject
+
+-(void)startScan;
+
+
+-(void)stopScan;
+
+@end
